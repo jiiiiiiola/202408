56a45d11156a089807af0aea01f227894b64fe6c
jiiiiiiola <ada20060306@gmail.com>
2024-08-02 16:03:14 +0800

    0802_4.01

--- workshop/resources/views/auth/signup.blade.php
+++ workshop/resources/views/auth/signup.blade.php
@@ -1,5 +1,11 @@
+@extends('layout.master') 
+@section('title', $title)
+@section('content')
+
+@include('components.socialButtons')
+
 <h1>{{ $title }}</h1> 
-<h1>{{ $sub title }}</h1> 
+<h2>{{ $sub_title }}</h2> 
 <div class="social"> 
     <a href="#">分享到 Facebook</a> 
     <a href="#">分享到 Twitter</a> 
@@ -7,3 +13,5 @@
 Email： <input type="text" name="email" placeholder="Email" > 
 密碼： <input type="password" name="password" placeholder="密碼" > 
 暱稱： <input type="text" name="nickname" placeholder="暱稱" > 
+
+@endsection

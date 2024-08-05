b6ef00341aa7cf1365487bf497438e0fae4989d1
jiiiiiiola <ada20060306@gmail.com>
2024-08-05 13:58:18 +0800

    0805.0158

--- workshop/resources/views/auth/signup.blade.php
+++ workshop/resources/views/auth/signup.blade.php
@@ -1,22 +1,17 @@
 <!-- 指定繼承 layout.master 母模板 --> 
 @extends('layout.master') 
-
 <!-- 傳送資料到母模板，並指定變數為 title --> 
 @section('title', $title) 
-
 <!-- 傳送資料到母模板，並指定變數為 content -->
 @section('content') 
-
 		<!-- Page Title
 		============================================= -->
 		<section class="page-title bg-transparent">
 			<div class="container">
 				<div class="page-title-row">
-
 					<div class="page-title-content">
 						<h1>註冊頁面</h1>
 					</div>
-
 					<nav aria-label="breadcrumb">
 						<ol class="breadcrumb">
 							<li class="breadcrumb-item"><a href="#">首頁</a></li>
@@ -24,21 +19,26 @@
 							<li class="breadcrumb-item active" aria-current="page">註冊頁面</li>
 						</ol>
 					</nav>
-
 				</div>
 			</div>
 		</section><!-- .page-title end -->
-
 		<!-- Content
 		============================================= -->
 		<section id="content">
 			<div class="content-wrap">
 				<div class="container">
-
 					<div >
-
-						<div class="form-result"></div>
-
+						<div class="form-result">
+						@if( $errors and count($errors))
+							<ul class="iconlist" data-username="envato" data-count="2">
+							@foreach( $errors->all() as $err )
+								<div class="alert text-center alert-danger">
+								{{$err}} 
+								</div>
+							@endforeach
+							</ul>
+						@endif 
+						</div>
 						<div class="row">
 							<div class="col-lg-12">
 								<form action="/user/auth/signup" method="post" >
@@ -65,19 +65,19 @@
 										
 										<div class="form-check form-check-inline">
 											@if( old('type') == 'G' )
-                                            @if( old('type') == 'G' )
 											<input class="form-check-input" type="radio" name="type" id="type" value="G" checked>
 											@else 
 											<input class="form-check-input" type="radio" name="type" id="type" value="G">
 											@endif
+											<label class="form-check-label text-transform-none" for="type">一般會員</label>
 										</div>
 										<div class="form-check form-check-inline">
 											@if( old('type') == 'A' )
-                                            @if( old('type') == 'A' )
 											<input class="form-check-input" type="radio" name="type" id="type" value="A" checked>
 											@else 
 											<input class="form-check-input" type="radio" name="type" id="type" value="A">
 											@endif
+											<label class="form-check-label text-transform-none" for="type">管理者</label>
 										</div>
 										
 									</div>
@@ -86,14 +86,9 @@
 									</div>
 								</form>
 							</div>
-
 						</div>
-
 					</div>
-
 				</div>
 			</div>
 		</section><!-- #content end -->
-
-
-@endsection 
+@endsection
